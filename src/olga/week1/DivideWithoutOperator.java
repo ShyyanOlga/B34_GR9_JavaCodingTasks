9f82d4fe21fea6c50b3e39647812d0a04a01e197
olgashyyan.uk@gmail.com <olgashyyan.uk@gmail.com>
2024-05-26 08:50:00 -0400

    Add division method in DivideWithoutOperator.java

--- src/olga/week1/DivideWithoutOperator.java
+++ src/olga/week1/DivideWithoutOperator.java
@@ -1,4 +1,24 @@
 package olga.week1;
 
 public class DivideWithoutOperator {
+    public static void main(String[] args) {
+
+        System.out.println("division(10,5) = " + division(10, 5));
+        System.out.println("division(15,3) = " + division(15, 3));
+
+    }
+    public static int division(int a, int b){
+        int c = a +b;
+        int result = 0;
+
+        while (c > b){
+            c = c -b;
+            result++;
+        }
+        return result;
+    }
+
 }
+/*
+
+ */
